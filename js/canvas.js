a8c06697ef3200a7e3d54c9e4f98e1a379d58215
aldousp <aldouspryce@gmail.com>
2017-07-04 12:20:31 -0400

    Add copy function.

--- js/canvas.js
+++ js/canvas.js
@@ -21,38 +21,53 @@ var rotationSpeed = 0;
 var btn_addWedge = document.getElementById("add-wedge");
 var wedge_input = document.getElementById("wedge-input");
 var wedge_list = document.getElementById("wedge-list");
-var templates = document.getElementById("templates");
 var spin_wheel = document.getElementById("spin-wheel");
 var clear_list = document.getElementById("clear-list");
+var copy_link = document.getElementById("copy-link");
+var templates = document.getElementById("templates");
 
 var mobile_break = 425;
 var desktop_canvas_size = 500;
 var mobile_canvas_size = 500;
 
-var name_list = [];
+var nameList = [];
 var name_lists = [
   ['E.Honda', 'Chun Li', 'M.Bison', 'Ryu'],
   ['Ringo', 'George', 'John', 'Paul'],
   ['Liu Kang', 'Sub-Zero', 'Sonya', 'Baraka']
 ];
 
-var blank_message = "Add some values in the pane on the right.";
+var blank_message = "Add some values in the pane.";
 var is_mobile = false;
 var last_width = 0;
+var clipboard = document.createElement('textarea');
 
 function setup() {
+  clipboard.style.opacity = 0;
+  clipboard.style.width = 0;
+  clipboard.style.height = 0;
+  document.body.appendChild(clipboard);
   CANVAS_MID_X = canvas.width / 2;
   CANVAS_MID_Y = canvas.height / 2;
   WHEEL_RADIUS = CANVAS_MID_X * WHEEL_PERCENTAGE;
   lastTime = Date.now();
-  // name_list
+  // nameList
   var saved_list = localStorage.getItem('wheel_items');
-  if (saved_list && !name_list.length) {
-    name_list = JSON.parse(saved_list);
+  if (saved_list && !nameList.length) {
+    nameList = JSON.parse(saved_list);
   }
 
-  if (!name_list || !name_list.length) {
-    name_list = name_lists[getRandomInt(0, name_lists.length)];
+  if (!nameList || !nameList.length) {
+    nameList = name_lists[getRandomInt(0, name_lists.length)];
+  }
+
+  var hash = window.location.hash;
+  var hashString = hash.substr(1, hash.length - 1);
+  if (hashString.length) {
+    var queryList = JSON.parse(hashString);
+    if (queryList.length) {
+      nameList = queryList;
+    }
   }
 
   checkIfMobile();
@@ -60,22 +75,24 @@ function setup() {
   registerInputListeners();
   renderList();
   loop();
+  refreshList();
 }
 
 function refreshList() {
   renderList();
-  localStorage.setItem('wheel_items', JSON.stringify(name_list));
+  localStorage.setItem('wheel_items', JSON.stringify(nameList));
+  window.location.hash = JSON.stringify(nameList);
 }
 
 function addItem() {
   if (wedge_input && !wedge_input.value) return;
-  name_list.push(wedge_input.value);
+  nameList.push(wedge_input.value);
   wedge_input.value = '';
   refreshList()
 }
 
 function removeItem(index) {
-  name_list.splice(index, 1);
+  nameList.splice(index, 1);
   refreshList();
 }
 
@@ -85,12 +102,18 @@ function spinWheel() {
 }
 
 function clearList() {
-  name_list = [];
+  nameList = [];
   refreshList();
 }
 
+function copyLink() {
+  clipboard.innerText = window.location.href;
+  clipboard.select();
+  document.execCommand('copy');
+  console.log('Running!');
+}
+
 function resizeCanvas() {
-  console.log(is_mobile);
   canvas.width = is_mobile ? mobile_canvas_size : desktop_canvas_size;
   canvas.height = is_mobile ? mobile_canvas_size : desktop_canvas_size;
   canvas.style.width = is_mobile ? mobile_canvas_size : desktop_canvas_size;
@@ -122,6 +145,7 @@ function registerInputListeners() {
   };
   spin_wheel.onclick = spinWheel;
   clear_list.onclick = clearList;
+  copy_link.onclick = copyLink;
 }
 
 function renderList() {
@@ -129,7 +153,7 @@ function renderList() {
   if (record_elem && wedge_list) {
     wedge_list.innerHTML = null;
     var i = 0;
-    name_list.forEach(function (elem) {
+    nameList.forEach(function (elem) {
       var template = record_elem.innerHTML;
       var instance = document.createElement('div');
       instance.innerHTML = template;
@@ -174,12 +198,12 @@ function loop() {
     wheelRotation += delta * rotationSpeed;
   }
   lastTime = Date.now();
-  wedgeSubdiv = 360 / name_list.length;
+  wedgeSubdiv = 360 / nameList.length;
 
   var x;
   var y;
   var wedgeRotation;
-  var list_len = name_list.length;
+  var list_len = nameList.length;
   for (var i = 0; i < list_len; i++) {
     context.beginPath();
     wedgeRotation = i * wedgeSubdiv + wheelRotation;
@@ -207,13 +231,13 @@ function loop() {
     context.fill();
   }
 
-  for (i = 0; i < name_list.length; i++) {
+  for (i = 0; i < nameList.length; i++) {
     context.beginPath();
     wedgeRotation = (i * wedgeSubdiv + wheelRotation) + wedgeSubdiv / 2;
     context.textAlign = "center";
-    context.font = canvas.width / 28 + "px Josefin Sans";
+    context.font = canvas.width / 24 + "px Josefin Sans";
     fillColor("#FFFFFF");
-    context.fillText(name_list[i], CANVAS_MID_X + Math.cos(degRad(wedgeRotation)) * (WHEEL_RADIUS * .75), CANVAS_MID_Y + Math.sin(degRad(wedgeRotation)) * (WHEEL_RADIUS * .75));
+    context.fillText(nameList[i], CANVAS_MID_X + Math.cos(degRad(wedgeRotation)) * (WHEEL_RADIUS * .75), CANVAS_MID_Y + Math.sin(degRad(wedgeRotation)) * (WHEEL_RADIUS * .75));
   }
 
   fillColor("#000000");
@@ -223,10 +247,10 @@ function loop() {
   context.lineTo(CANVAS_MID_X, canvas.height / 18);
   context.fill();
 
-  if (!name_list.length) {
+  if (!nameList.length) {
     context.beginPath();
     context.textAlign = "center";
-    context.font = canvas.width / 28  + "px Josefin Sans";
+    context.font = canvas.width / 24  + "px Josefin Sans";
     fillColor("#000000");
     context.fillText(blank_message, CANVAS_MID_X, CANVAS_MID_Y);
   } else {
